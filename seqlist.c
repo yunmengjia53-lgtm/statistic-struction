d16c71c50130f24d352204ce369f68419a90bcae
yunmengjia53-lgtm <yunmengjia53@gmail.com>
2026-07-06 11:50:38 +0800

    Initialize seqlist structure and fix errors
    Fix syntax errors and include necessary headers.

--- seqlist.c
+++ seqlist.c
@@ -1,1 +1,11 @@
-
+#include 'studio.h'
+#define MAXSIZE 100
+typedef int elemtype
+typedef struct ｛
+elemtype data[MAXSIZE];
+int length;
+｝seqlist;
+viod initlist(seqlist* L)｛
+L-›length = 0;
+｝;
+printf('%d', L-›length)
